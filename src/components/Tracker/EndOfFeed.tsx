e4bb7f8fabe153d280131dd5cc6c3e1c81b71586
pitchstacks <andrew@pitchstacks.com>
2023-02-12 15:50:26 -0500

    feed error fix

--- src/components/Tracker/EndOfFeed.tsx
+++ src/components/Tracker/EndOfFeed.tsx
@@ -9,7 +9,7 @@ const EndOfFeed:React.FC<EndOfFeedProps> = () => {
     
     return (
         <Stack mt={7} mb={5} pl="32%">
-            <Text color="gray.500">👏 You've seen it all today!</Text>
+            <Text color="gray.500">{`👏 You've seen it all today!`}</Text>
         </Stack>
     );
 }
