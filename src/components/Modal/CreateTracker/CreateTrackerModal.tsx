6fc35d69cdd99580548cb6e0116a5ec4acde6ee0
pitchstacks <andrew@pitchstacks.com>
2023-02-13 03:57:56 -0500

    first commit

--- src/components/Modal/CreateTracker/CreateTrackerModal.tsx
+++ src/components/Modal/CreateTracker/CreateTrackerModal.tsx
@@ -1,5 +1,5 @@
 import { auth, firestore } from '@/src/firebase/configApp';
-import clickDirectory from '@/src/hooks/ClickDirectory';
+import ClickDirectory from '@/src/hooks/ClickDirectory';
 import { Text, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Box, Input, Stack, Checkbox, Flex, Icon } from '@chakra-ui/react';
 import { doc, getDoc, runTransaction, serverTimestamp, setDoc } from 'firebase/firestore';
 import { useRouter } from 'next/router';
@@ -24,7 +24,7 @@ const CreateTrackerModal:React.FC<CreateTrackerModalProps> = ({ open, closecreat
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
     const router = useRouter();
-    const { makeMenuOpen } = clickDirectory();
+    const { makeMenuOpen } = ClickDirectory();
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         // count we have left
